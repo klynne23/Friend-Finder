fbd733029c69c1c96519404dd73b2a3a15502fcf
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-01-09 19:00:33 -0500

    working code

---
+++ friends.js
@@ -0,0 +1,27 @@
+// holds the array of objects for each Friend
+
+var friends = [
+    {
+        name: "Joe",
+        photo: "https://i.pinimg.com/originals/2d/5a/61/2d5a6180dff8ce671a4d29d64a19f64f.jpg",
+        scores: [1, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+    },
+    {
+        name: "Sal",
+        photo: "https://frostsnow.com/uploads/biography/2016/04/19/xsal-vulcano.jpg.pagespeed.ic.DckQH32ngF.webp",
+        scores: [2, 4, 2, 3, 1, 3, 1, 2, 3, 5]
+    },
+    {
+        name: "Q",
+        photo: "https://i.static.cdn.trutv.com/pictures/8306e4e2/images/series/impractical-jokers/cast/brian-q-quinn_75.jpg",
+        scores: [2, 4, 2, 3, 1, 3, 1, 2, 3, 5]
+    },
+    {
+        name: "Murr",
+        photo: "https://marriedbiography.com/wp-content/uploads/2017/12/James-Murray.jpg",
+        scores: [2, 4, 2, 3, 1, 3, 1, 2, 3, 5]
+    }
+]
+
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,9 +1,50 @@
-const express = require('express');
-const app = express();
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// require our friends array
+var friendsArray = require("./friends")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsingclear
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+app.get("/survey", (req, res) => {
+    res.sendFile(path.join(__dirname, "survey.html"));
+})
+
+app.get("/friends", function(req, res) {
+    return res.json(friendsArray);
+  });
+
+app.post("/friends", function(req, res){
+
+    var newFriend = req.body;
+    console.log(newFriend.answers);
+
+    // do the logic to figure out which friend is the best... 
+
+
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
